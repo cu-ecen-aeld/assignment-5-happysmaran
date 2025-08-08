08972483a43929d027dc89086f861c7953e922db
smaran <smaranvallabhaneni@gmail.com>
2025-08-08 13:17:33 +0530

    fix aesd-assignments.mk again again

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = da36285cae02b3e696ab2e4bf3560eddf0139201
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-happysmaran.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-happysmaran.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
