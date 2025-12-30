4660b3bd0dbb2b5c579517bac54b34c73bc27c0e
happysmaran <smaranvallabhaneni@gmail.com>
2025-12-30 06:22:15 -0500

    Ass. 7 Pt. 2 - Inside, we both know what's been going on

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,20 +1,20 @@
-LDD_VERSION = 0fa6848cc5ffe3d06b065d2c6c9713cccbf20038
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-happysmaran.git
+LDD_VERSION = '0fa6848cc5ffe3d06b065d2c6c9713cccbf20038'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-happysmaran.git'
 LDD_SITE_METHOD = git
 
-# Ensure the kernel is built before we try to build modules
-LDD_DEPENDENCIES = linux
-
 define LDD_BUILD_CMDS
-    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(@D)/scull modules
-    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(@D)/misc-modules modules
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/scull modules
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/misc-modules modules
 endef
 
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
-	$(INSTALL) -m 0644 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
-	$(INSTALL) -m 0644 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
-	$(INSTALL) -m 0644 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0644 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/scull.ko
+	$(INSTALL) -D -m 0644 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/faulty.ko
+	$(INSTALL) -D -m 0644 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/hello.ko
 endef
 
 $(eval $(generic-package))
